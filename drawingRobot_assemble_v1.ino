8dd12eadc5b1dabb48e0a6a2c378a5fdf0ecc785
YangJipsa <109192582+yangjipsa@users.noreply.github.com>
2024-05-23 16:00:56 +0900

    Update drawingRobot_assemble_v1.ino

--- drawingRobot_assemble_v1.ino
+++ drawingRobot_assemble_v1.ino
@@ -10,39 +10,21 @@ Servo servo_lift;
 
 int angle = 0;
 
-void setup() {
+void setup()
+{
   servo_l.attach(ServoL);
   servo_r.attach(ServoR);
   servo_lift.attach(ServoLift);
 
-  angle = 0;
-  servo_l.write(angle);
-  servo_r.write(angle);
-  servo_lift.write(angle);
   delay(1000);
 
-  angle = 180;
+  angle = 90;
   servo_l.write(angle);
   servo_r.write(angle);
   servo_lift.write(angle);
-  delay(1000);
 }
 
-void loop() {
-  for (int i = 0 ; i < 4 ; i++)
-  { // angle : 30 60 90 120
-    angle = 30*(i+1);
-    servo_l.write(angle);
-    servo_r.write(angle);
-    servo_lift.write(angle);
-    delay(1000);
-  }
-  for (int i = 0 ; i < 4 ; i++)
-  { // angle : 150 120 90 60
-    angle = 30*(5-i);
-    servo_l.write(angle);
-    servo_r.write(angle);
-    servo_lift.write(angle);
-    delay(1000);
-  }  
+void loop() 
+{
+  
 }
